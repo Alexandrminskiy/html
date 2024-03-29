9d02227219ff9184d401703de5f8f73aea299d2b
Alexandrminskiy <alexandrminskiy@gmail.com>
2024-03-29 15:46:05 +0300

    123

--- Test_dataset/script.js
+++ Test_dataset/script.js
@@ -1,2 +1,15 @@
 let btn = document.querySelector('#btn');
 let radioElement = document.querySelectorAll('[name="goods"]');
+
+const choiseGood = { value: "", price: 0, size: "", };
+
+btn.addEventListener('click', () => {
+    radioElement.forEach(e => {
+        if (e.checked) {
+            choiseGood.value = e.value;
+            choiseGood.price = +(e.dataset.price);
+            choiseGood.size = e.dataset.size;
+        }
+    })
+    console.log(choiseGood);
+})
