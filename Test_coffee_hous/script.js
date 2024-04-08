e8936da8047caa7599f207ae6b998368ee330020
Alexandrminskiy <alexandrminskiy@gmail.com>
2024-04-08 16:24:36 +0300

    123

--- Test_coffee_hous/script.js
+++ Test_coffee_hous/script.js
@@ -1,40 +1,58 @@
-let checkBtn = document.querySelectorAll('[name="coffee"]');
+let checkBtn = document.querySelectorAll('[name="coffe"]');
 let result = document.querySelector('.result');
-let count = document.querySelectorAll('.input-value');
-// let counter = document.querySelectorAll('[name="qwe"]')
+let inputCount = document.querySelectorAll('.input-value');
+// let inputCount = document.querySelectorAll('.count');
 
 let user = {
-	firstName: "",
-	secondName: "",
-	amount: 0,
+    firstName: "",
+    secondName: "",
+    amount: 0,
 }
 
 
-function calcAmount (){
+function calcAmount() {
     let total = 0;
-    for(let j = 0; j < checkBtn.length;j++){
-        if(checkBtn[j].checked){
-            total += +(checkBtn[j].dataset.price)* +(count[j].value)
-            console.log(checkBtn[j].dataset.price);
-            console.log(count[j].value);
+    for (let j = 0; j < checkBtn.length; j++) {
+        if (checkBtn[j].checked) {
+            total += +(checkBtn[j].dataset.price) * +(inputCount[j].value)
+            // console.log(checkBtn[j].dataset.price);
+            // console.log(inputCount[j].value);
         }
     }
     return total
 }
 
-
-
-
-
-
-document.addEventListener('change', () => {
-    let total = 0;
-    for (const e of checkBtn) {
-        if (e.checked) {
-            total += +(e.value);
-           calcAmount ()
+for (let i = 0; i < checkBtn.length; i++) {
+    checkBtn[i].addEventListener('change', () => {
+        if (checkBtn[i].checked) {
+            inputCount[i].value = 1;
         }
-    } 
-    result.textContent = total
-});
+        else {
+            inputCount[i].value = 0;
+        }
+        user.amount = calcAmount();
+        result.textContent = user.amount;
+    })
+}
+for (let i = 0; i < inputCount.length; i++) {
+    inputCount[i].addEventListener('change', () => {
+        user.amount = calcAmount();
+        result.textContent = user.amount;
+    })
+}
+
+document.addEventListener("click", () => {
+    user.amount = document.querySelector(".result").textContent;
+})
+
+// document.addEventListener('change', () => {
+//     let total = 0;
+//     for (const e of checkBtn) {
+//         if (e.checked) {
+//             total += +(e.value);
+//             calcAmount()
+//         }
+//     }
+//     result.textContent = total
+// });
 
